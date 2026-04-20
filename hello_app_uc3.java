63aadf55db292923fbf690d94ddda08f0d047df4
Pratik Mukherjee <pratikmukherjee@Pratiks-MacBook-Air-10.local>
2026-04-21 00:34:43 +0530

    new file

---
+++ hello_app_uc3.java
@@ -0,0 +1,9 @@
+public class hello_app_uc3{
+    public static void main(String[] args){
+        String name="World";
+        if(args.length>0){
+            name=args[0];
+        }
+        System.out.println("hello "+name);
+    }
+}
